30ceb53a8a018492b413f855592757082a825907
Arthur D <dantas.arthur.contact@gmail.com>
2023-06-27 16:42:22 -0300

    Exercicios adicionados

--- exercicios/ex001/horario/script.js
+++ exercicios/ex001/horario/script.js
@@ -1,8 +1,7 @@
 var real = new Date()
 var hora = real.getHours()
-var min = real.getMinutes()
+var min = real.getMinutes().toString().padStart(2, '0')
 
-var back = window.document.getElementById('bg')
 var subtitle = window.document.getElementById('subtitle')
 var imagem = window.document.getElementById('img')
 
@@ -11,10 +10,13 @@ subtitle.innerText = `Agora são exatamente ${hora}:${min}`
 function change_img(imagem) {
     if (hora > 6 && hora < 12) {
         imagem.src = 'assets/dia.jpg'
+        document.body.style.backgroundColor = 'lightskyblue'
     } else if (hora > 12 && hora < 18) {
         imagem.src = 'assets/tarde.jpg'
+        document.body.style.backgroundColor = 'coral'
     } else {
         imagem.src = 'assets/noite.jpg'
+        document.body.style.backgroundColor = 'midnightblue'
     }
 }
 
---
+++ exercicios/ex001/idade/script.js
@@ -0,0 +1,10 @@
+var atual = new Date()
+
+var nasc = window.document.getElementById('ano')
+console.log(nasc)
+var masc = window.document.getElementById('mas')
+var femi = window.document.getElementById('fem')
+
+function analisar() {
+    window.alert('Olá')
+}
