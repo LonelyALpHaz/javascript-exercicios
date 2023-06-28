6da13bbedfd7b9c0ffefedf9e72b1f3f487956a5
Arthur D <dantas.arthur.contact@gmail.com>
2023-06-28 18:40:48 -0300

    Exercício adicionado

--- exercicios/ex001/idade/script.js
+++ exercicios/ex001/idade/script.js
@@ -1,10 +1,44 @@
 var atual = new Date()
-
-var nasc = window.document.getElementById('ano')
-console.log(nasc)
-var masc = window.document.getElementById('mas')
-var femi = window.document.getElementById('fem')
+var ano = atual.getFullYear()
+var img = window.document.getElementById('img')
 
 function analisar() {
-    window.alert('Olá')
+    var checkboxes = document.getElementsByName('sexo')
+    var label = document.getElementById('label')
+    var nasc = document.getElementById('nasc')
+    var idade = ano - nasc.value
+    var sexoSelecionado = [];
+    console.log(ano)
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            sexoSelecionado.push(checkboxes[i].value);
+            if (sexoSelecionado.toString() == 'HOMEM' && idade < 13) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/menino.jpg'
+            } else if (sexoSelecionado.toString() == 'HOMEM' && idade > 13 && idade < 18) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/hjovem.jpg'
+            } else if (sexoSelecionado.toString() == 'HOMEM' && idade > 18 && idade < 60) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/hadulto.jpg'
+            } else if (sexoSelecionado.toString() == 'HOMEM' && idade > 60) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/hvelho.jpg'
+            } else if (sexoSelecionado.toString() == 'MULHER' && idade < 13) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/menina.jpg'
+            } else if (sexoSelecionado.toString() == 'MULHER' && idade > 13 && idade < 18) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/mjovem.jpg'
+            } else if (sexoSelecionado.toString() == 'MULHER' && idade > 18 && idade < 60) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/madulta.jpg'
+            } else if (sexoSelecionado.toString() == 'MULHER' && idade > 60) {
+                label.innerText = `De acordo com nossas análise você é um ${sexoSelecionado} de ${idade} anos.`
+                img.src = 'assets/mvelha.jpg'
+            } else if (sexoSelecionado.toString() == 'HOMEM' && sexoSelecionado.toString() == 'MULHER') {
+                label.innerText = `Por favor, selecione apenas um sexo.`
+            }
+        }
+    }
 }
